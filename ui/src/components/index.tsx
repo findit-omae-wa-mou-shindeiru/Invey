f093af64bb4f05f235bfce9309bf88de63cd0389
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2022-05-26 12:29:07 +0700

    feat: add baseline for analytics form page

--- ui/src/components/index.tsx
+++ ui/src/components/index.tsx
@@ -1,20 +1,21 @@
-import Navbar from './shared/navbar';
-import LandingContent from './landing/content';
-import Nav from './dashboard/shared/nav';
-import Sidebar from './dashboard/shared/sidebar';
-import FilterSelection from './dashboard/explore/filter-selection';
-import SurveyBox from './dashboard/shared/survey-box';
-import UserInfo from './shared/user-info';
-import SurveyChart from './dashboard/manage/survey-chart';
-import StatsSummary from './dashboard/manage/stats-summary';
-import FormNav from './form/shared/form-nav';
-import AddQuestionSidebar from './form/survey/add-question-sidebar';
-import ShortAnswerQuestion from './form/survey/short-answer-question';
-import ParagraphQuestion from './form/survey/paragraph-question';
-import MultipleChoiceQuestion from './form/survey/multiple-choice-question';
-import CheckboxQuestion from './form/survey/checkbox-question';
-import DropdownQuestion from './form/survey/dropdown-question';
-import LinearScaleQuestion from './form/survey/linear-scale-question';
+import Navbar from "./shared/navbar";
+import LandingContent from "./landing/content";
+import Nav from "./dashboard/shared/nav";
+import Sidebar from "./dashboard/shared/sidebar";
+import FilterSelection from "./dashboard/explore/filter-selection";
+import SurveyBox from "./dashboard/shared/survey-box";
+import UserInfo from "./shared/user-info";
+import SurveyChart from "./dashboard/manage/survey-chart";
+import StatsSummary from "./dashboard/manage/stats-summary";
+import FormNav from "./form/shared/form-nav";
+import AddQuestionSidebar from "./form/survey/add-question-sidebar";
+import ShortAnswerQuestion from "./form/survey/short-answer-question";
+import ParagraphQuestion from "./form/survey/paragraph-question";
+import MultipleChoiceQuestion from "./form/survey/multiple-choice-question";
+import CheckboxQuestion from "./form/survey/checkbox-question";
+import DropdownQuestion from "./form/survey/dropdown-question";
+import LinearScaleQuestion from "./form/survey/linear-scale-question";
+import AnalyticsSidebar from "./form/analytics/analytics-sidebar";
 
 export {
   Navbar,
@@ -34,4 +35,5 @@ export {
   CheckboxQuestion,
   DropdownQuestion,
   LinearScaleQuestion,
+  AnalyticsSidebar,
 };
