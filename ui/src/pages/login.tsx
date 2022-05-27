56de13990ff7f2326cf9579e7bd97100427acd19
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2022-05-27 15:25:03 +0700

    feat: integrate login and register functionality with BE

--- ui/src/pages/login.tsx
+++ ui/src/pages/login.tsx
@@ -1,29 +1,45 @@
-import { Auth } from 'layout';
+import { useRouter } from 'next/router';
+import { Auth } from "layout";
+import { ApiProxy } from "services";
 
 const Login = () => {
+  const router = useRouter();
+
   const content = {
-    title: 'Welcome Back',
-    subtitle: 'Hello, there',
+    title: "Welcome Back",
+    subtitle: "Hello, there",
     inputs: [
       {
-        key: 'email',
-        placeholder: 'Email',
-        defaultValue: '',
-        type: 'email',
+        key: "email",
+        placeholder: "Email",
+        defaultValue: "",
+        type: "email",
       },
       {
-        key: 'password',
-        placeholder: 'Password',
-        defaultValue: '',
-        type: 'password',
+        key: "password",
+        placeholder: "Password",
+        defaultValue: "",
+        type: "password",
       },
     ],
-    btn: 'Login',
+    btn: "Login",
     linkLbl1: "Don't have an account? ",
-    linkLbl2: 'Register here',
-    link: '/register',
-    onSubmit: (params: Object) =>
-      console.log('login', params),
+    linkLbl2: "Register here",
+    link: "/register",
+    onSubmit: async (params: Object) => {
+      const { err } = await ApiProxy.getInstance().login(
+        params as {
+          email: string;
+          password: string;
+        }
+      );
+
+      if (err) {
+        alert(err);
+      } else {
+        router.push("/dashboard/explore");
+      }
+    },
   };
 
   return <Auth content={content} />;
--- ui/src/pages/register.tsx
+++ ui/src/pages/register.tsx
@@ -1,45 +1,62 @@
-import { Auth } from 'layout';
+import { useRouter } from "next/router";
+import { Auth } from "layout";
+import { ApiProxy } from "services";
 
 const Register = () => {
+  const router = useRouter();
+
   const content = {
-    title: 'Get Started for free',
-    subtitle: 'Hello, there',
+    title: "Get Started for free",
+    subtitle: "Hello, there",
     inputs: [
       {
-        key: 'firstName',
-        placeholder: 'First Name',
-        defaultValue: '',
-        type: 'text',
+        key: "firstname",
+        placeholder: "First Name",
+        defaultValue: "",
+        type: "text",
       },
       {
-        key: 'lastName',
-        placeholder: 'Last Name',
-        defaultValue: '',
-        type: 'text',
+        key: "secondname",
+        placeholder: "Last Name",
+        defaultValue: "",
+        type: "text",
       },
       {
-        key: 'email',
-        placeholder: 'Email',
-        defaultValue: '',
-        type: 'email',
+        key: "email",
+        placeholder: "Email",
+        defaultValue: "",
+        type: "email",
       },
       {
-        key: 'password',
-        placeholder: 'Password',
-        defaultValue: '',
-        type: 'password',
+        key: "password",
+        placeholder: "Password",
+        defaultValue: "",
+        type: "password",
       },
     ],
-    btn: 'Register',
-    linkLbl1: 'Already have an account? ',
-    linkLbl2: 'login here',
-    link: '/login',
-    onSubmit: (params: Object) => console.log("register", params),
+    btn: "Register",
+    linkLbl1: "Already have an account? ",
+    linkLbl2: "login here",
+    link: "/login",
+    onSubmit: async (params: Object) => {
+      const { err } = await ApiProxy.getInstance().register(
+        params as {
+          firstname: string;
+          secondname: string;
+          email: string;
+          password: string;
+        }
+      );
+
+      if (err) {
+        alert(err);
+      } else {
+        router.push("/dashboard/explore");
+      }
+    },
   };
 
-  return (
-    <Auth content={content} />
-  );
+  return <Auth content={content} />;
 };
 
 export default Register;
