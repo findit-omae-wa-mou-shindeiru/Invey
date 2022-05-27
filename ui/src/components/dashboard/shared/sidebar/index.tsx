1c9961ddc0c945ca1dc10dd67e3c5d01c1374bbd
Malik Rafsanjani <pro.malikakbar2357@gmail.com>
2022-05-28 00:03:16 +0700

    feat: change settings section dashboard icon

--- ui/src/components/dashboard/shared/sidebar/index.tsx
+++ ui/src/components/dashboard/shared/sidebar/index.tsx
@@ -1,7 +1,7 @@
-import Link from 'next/link';
-import styles from './index.module.css';
-import { useRouter } from 'next/router';
-import { Cross } from 'hamburger-react';
+import Link from "next/link";
+import styles from "./index.module.css";
+import { useRouter } from "next/router";
+import { Cross } from "hamburger-react";
 
 const Sidebar = ({
   className,
@@ -16,102 +16,72 @@ const Sidebar = ({
 
   const links = [
     {
-      url: '/dashboard/explore',
-      label: 'Explore Surveys',
-      img: '/explore',
+      url: "/dashboard/explore",
+      label: "Explore Surveys",
+      img: "/explore",
     },
     {
-      url: '/dashboard/manage',
-      label: 'Manage Surveys',
-      img: '/manage',
+      url: "/dashboard/manage",
+      label: "Manage Surveys",
+      img: "/manage",
     },
     {
-      url: '/dashboard/settings',
-      label: 'Settings',
-      img: '/setting',
+      url: "/dashboard/settings",
+      label: "Settings",
+      img: "/settings",
     },
     {
-      url: '/dashboard/my-points',
-      label: 'My Points',
-      img: '/my-points',
-    }
+      url: "/dashboard/my-points",
+      label: "My Points",
+      img: "/my-points",
+    },
   ];
 
   return (
     <>
-      <div
-        className={
-          styles.container + ' d-none d-xxl-block'
-        }
-      ></div>
+      <div className={styles.container + " d-none d-xxl-block"}></div>
       <div
         className={
           styles.container +
-          ' ' +
+          " " +
           styles.whiteBG +
-          ` ${
-            show ? '' : 'd-none'
-          } d-xxl-block position-fixed top-0 start-0` +
+          ` ${show ? "" : "d-none"} d-xxl-block position-fixed top-0 start-0` +
           className
         }
       >
         <div className="d-flex justify-content-end d-xxl-none mt-2 px-2">
           <Cross toggled={true} toggle={close} />
         </div>
-        <div
-          className={
-            styles.topContainer +
-            ' d-flex justify-content-center'
-          }
-        >
+        <div className={styles.topContainer + " d-flex justify-content-center"}>
           <div className={styles.imgContainer}>
-            <img
-              src="/logo-white.png"
-              alt="logo"
-            />
+            <img src="/logo-white.png" alt="logo" />
           </div>
         </div>
-        <div
-          className={
-            styles.linksContainer +
-            ' d-flex flex-column'
-          }
-        >
+        <div className={styles.linksContainer + " d-flex flex-column"}>
           {links.map((link) => {
             return (
-              <Link
-                href={link.url}
-                key={link.url}
-              >
+              <Link href={link.url} key={link.url}>
                 <a
                   className={
                     (router.pathname === link.url
                       ? styles.linkActive
-                      : styles.link) +
-                    ' px-5 py-4 mb-3'
+                      : styles.link) + " px-5 py-4 mb-3"
                   }
                 >
                   <div className="d-flex align-items-center">
-                    <div
-                      className={
-                        styles.iconContainer
-                      }
-                    >
+                    <div className={styles.iconContainer}>
                       <img
                         src={
                           link.img +
-                          (router.pathname ===
-                          link.url
-                            ? '-active'
-                            : '-inactive') +
-                          '.svg'
+                          (router.pathname === link.url
+                            ? "-active"
+                            : "-inactive") +
+                          ".svg"
                         }
-                        alt={link.label + ' icon'}
+                        alt={link.label + " icon"}
                       />
                     </div>
-                    <div className="ms-2">
-                      {link.label}
-                    </div>
+                    <div className="ms-2">{link.label}</div>
                   </div>
                 </a>
               </Link>
